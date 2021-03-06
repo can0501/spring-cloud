fc49863f7ec812eb31925e4ad9089a03cf6f5089
can <493717055@qq.com>
2021-03-06 17:01:00 +0800

    asdasd

--- dubbo/dubbo-provider/src/main/java/com/example/dubbo_provider/DubboProviderApplication.java
+++ dubbo/dubbo-provider/src/main/java/com/example/dubbo_provider/DubboProviderApplication.java
@@ -1,19 +1,13 @@
 package com.example.dubbo_provider;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
+ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 
 @SpringBootApplication
 
 public class DubboProviderApplication {
-
 
 
     public static void main(String[] args) {
